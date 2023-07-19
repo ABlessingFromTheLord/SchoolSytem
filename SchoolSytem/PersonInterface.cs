995598d62d7fe38fa96eab4aa366bd0b4b5b0e49
Samwel Suleiman <94542984+ABlessingFromTheLord@users.noreply.github.com>
2023-07-19 19:20:58 +0200

    Added interfaces to implement base classes

---
+++ SchoolSytem/PersonInterface.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace SchoolSytem
+{
+	public interface Person
+	{
+		// First name of person
+		public string FirstName { get; set; }
+		// Last name of person
+		public string LastName { get; set; }
+		// Address of person
+		public string Address { get; set; }
+		// Telefon number of person
+		public string Telefon { get; set; }
+		// Faculty studing
+		public string FacultyName { get; set; }
+		// Number of subject studying or teaching
+		// For teachers teaching, for students studying
+		public int Subjects { get; set; }
+	}
+}
+
