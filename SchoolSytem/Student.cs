7b1acb02a20f6257259f8c9b76e7ff05556f2379
ABlessingFromTheLord <94542984+ABlessingFromTheLord@users.noreply.github.com>
2023-07-21 00:49:14 +0200

    Implemented Student.cs

---
+++ SchoolSytem/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSytem
+{
+    public class Student : BasePerson
+    {
+        #region Fields
+        #endregion
+
+        #region Constructor
+        public Student(string FirstName, string LastName, string Gender, int Age, string Id)
+            :base(FirstName, LastName, Gender, Age, Id)
+        {
+        
+        }
+        #endregion
+    }
+}
