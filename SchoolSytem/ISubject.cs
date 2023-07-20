256d79f0f154a3fda9e2cbb7cdad975b97690ecb
ABlessingFromTheLord <94542984+ABlessingFromTheLord@users.noreply.github.com>
2023-07-21 00:05:45 +0200

    Refactored interfaces

---
+++ SchoolSytem/ISubject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace SchoolSytem
+{
+	public interface ISubject
+	{
+		// Name of subject
+		public string SubjectName{ get; set; }
+		// Credit points obtained from studying this subject
+		public int CreditPoints { get; set; }
+		// Workload of this suject
+		public int Workload { get; set; }
+		// Amount of teachers teaching this subject
+		public int NumberOfTeachers { get; set; }
+		// Amount of students taking this subject
+		public int NumberOfStudents { get; set; }
+	}
+}
+
---
+++ SchoolSytem/Person.cs
@@ -0,0 +1,75 @@
+﻿using SchoolSytem.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSytem
+{
+    /// <summary>
+    /// This class represents a person 
+    /// </summary>
+    public abstract class Person : IPerson
+    {
+        #region Fields
+
+        #endregion
+
+        #region Constructor
+        public Person()
+        {
+
+        }
+        #endregion
+
+        #region Properties
+        public string FirstName { get; set;}
+        public string LastName { get; set; }
+        public string Id { get; set; }
+        public string Role { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string Telefon { get; set; }
+        public string FacultyName { get; set; }
+        public int Subjects { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        #endregion
+
+        #region Methods
+        public string GetAddress(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFacultyName(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFirstName(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetLastName(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetName(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetRole(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetTelefon(string id)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
