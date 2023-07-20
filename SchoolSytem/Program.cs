4e00a1150d224e4be4331d488b8d5ddad6cc6b47
ABlessingFromTheLord <94542984+ABlessingFromTheLord@users.noreply.github.com>
2023-07-21 00:49:34 +0200

    Inserted trial instances for student

--- SchoolSytem/Program.cs
+++ SchoolSytem/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SchoolSytem;
+
+#region Student Instances
+Student student1 = new Student("John", "Doe", "Male", 20, "ID001");
+Student student2 = new Student("Jane", "Smith", "Female", 19, "ID002");
+Student student3 = new Student("Michael", "Johnson", "Male", 22, "ID003");
+Student student4 = new Student("Emily", "Brown", "Female", 21, "ID004");
+Student student5 = new Student("James", "Lee", "Male", 18, "ID005");
+Student student6 = new Student("Emma", "Wilson", "Female", 23, "ID006");
+Student student7 = new Student("Daniel", "Taylor", "Male", 20, "ID007");
+Student student8 = new Student("Olivia", "Anderson", "Female", 19, "ID008");
+Student student9 = new Student("William", "Martinez", "Male", 22, "ID009");
+Student student10 = new Student("Ava", "Garcia", "Female", 21, "ID010");
+Student student11 = new Student("Liam", "Lopez", "Male", 18, "ID011");
+Student student12 = new Student("Sophia", "Hernandez", "Female", 23, "ID012");
+Student student13 = new Student("Noah", "Gonzalez", "Male", 20, "ID013");
+Student student14 = new Student("Isabella", "Miller", "Female", 19, "ID014");
+Student student15 = new Student("James", "Davis", "Male", 22, "ID015");
+Student student16 = new Student("Oliver", "Rodriguez", "Male", 18, "ID016");
+Student student17 = new Student("Charlotte", "Smith", "Female", 21, "ID017");
+Student student18 = new Student("Elijah", "Johnson", "Male", 20, "ID018");
+Student student19 = new Student("Amelia", "Brown", "Female", 19, "ID019");
+Student student20 = new Student("Benjamin", "Lee", "Male", 22, "ID020");
+Student student21 = new Student("Mia", "Wilson", "Female", 21, "ID021");
+Student student22 = new Student("Mason", "Taylor", "Male", 18, "ID022");
+Student student23 = new Student("Evelyn", "Anderson", "Female", 23, "ID023");
+Student student24 = new Student("Alexander", "Martinez", "Male", 20, "ID024");
+Student student25 = new Student("Harper", "Garcia", "Female", 19, "ID025");
+#endregion
+
 
