863063082d2d1b40a7b69b70a69d0e1df39d7724
ABlessingFromTheLord <94542984+ABlessingFromTheLord@users.noreply.github.com>
2023-07-21 00:57:40 +0200

    Added instances

--- SchoolSytem/Program.cs
+++ SchoolSytem/Program.cs
@@ -1,5 +1,53 @@
 ﻿using SchoolSytem;
 
+#region Faculty Instances
+Faculty faculty1 = new Faculty
+{
+    FacultyName = "Computer Science",
+    SubjectsOffered = 10,
+    ChairPersonName = "John Smith",
+    DeanName = "Jane Doe",
+    Member = "Michael Johnson"
+};
+
+Faculty faculty2 = new Faculty
+{
+    FacultyName = "Electrical Engineering",
+    SubjectsOffered = 8,
+    ChairPersonName = "Emily Brown",
+    DeanName = "James Lee",
+    Member = "Emma Wilson"
+};
+
+Faculty faculty3 = new Faculty
+{
+    FacultyName = "Mechanical Engineering",
+    SubjectsOffered = 7,
+    ChairPersonName = "Daniel Taylor",
+    DeanName = "Olivia Anderson",
+    Member = "William Martinez"
+};
+
+Faculty faculty4 = new Faculty
+{
+    FacultyName = "Physics",
+    SubjectsOffered = 6,
+    ChairPersonName = "Ava Garcia",
+    DeanName = "Liam Lopez",
+    Member = "Sophia Hernandez"
+};
+
+Faculty faculty5 = new Faculty
+{
+    FacultyName = "Mathematics",
+    SubjectsOffered = 9,
+    ChairPersonName = "Noah Gonzalez",
+    DeanName = "Isabella Miller",
+    Member = "James Davis"
+};
+
+#endregion
+
 #region Student Instances
 Student student1 = new Student("John", "Doe", "Male", 20, "ID001");
 Student student2 = new Student("Jane", "Smith", "Female", 19, "ID002");
