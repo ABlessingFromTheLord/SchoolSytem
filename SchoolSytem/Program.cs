3ce0307fd376101c296ef2f2eb46b98590b3061d
ABlessingFromTheLord <94542984+ABlessingFromTheLord@users.noreply.github.com>
2023-10-25 17:57:04 +0200

    readjusted sample instances and added them in List

--- SchoolSytem/Program.cs
+++ SchoolSytem/Program.cs
@@ -59,21 +59,29 @@ Student student7 = new Student("Daniel", "Taylor", "Male", 20, "ID007");
 Student student8 = new Student("Olivia", "Anderson", "Female", 19, "ID008");
 Student student9 = new Student("William", "Martinez", "Male", 22, "ID009");
 Student student10 = new Student("Ava", "Garcia", "Female", 21, "ID010");
-Student student11 = new Student("Liam", "Lopez", "Male", 18, "ID011");
-Student student12 = new Student("Sophia", "Hernandez", "Female", 23, "ID012");
-Student student13 = new Student("Noah", "Gonzalez", "Male", 20, "ID013");
-Student student14 = new Student("Isabella", "Miller", "Female", 19, "ID014");
-Student student15 = new Student("James", "Davis", "Male", 22, "ID015");
-Student student16 = new Student("Oliver", "Rodriguez", "Male", 18, "ID016");
-Student student17 = new Student("Charlotte", "Smith", "Female", 21, "ID017");
-Student student18 = new Student("Elijah", "Johnson", "Male", 20, "ID018");
-Student student19 = new Student("Amelia", "Brown", "Female", 19, "ID019");
-Student student20 = new Student("Benjamin", "Lee", "Male", 22, "ID020");
-Student student21 = new Student("Mia", "Wilson", "Female", 21, "ID021");
-Student student22 = new Student("Mason", "Taylor", "Male", 18, "ID022");
-Student student23 = new Student("Evelyn", "Anderson", "Female", 23, "ID023");
-Student student24 = new Student("Alexander", "Martinez", "Male", 20, "ID024");
-Student student25 = new Student("Harper", "Garcia", "Female", 19, "ID025");
+
+
+List<Student> students = new List<Student>();
+students.Add(student1);
+students.Add(student2);
+students.Add(student3);
+students.Add(student4);
+students.Add(student5);
+students.Add(student6);
+students.Add(student7);
+students.Add(student8);
+students.Add(student9);
+students.Add(student10);
+
+
+for (int i = 0; i < students.Count; i++) {
+    Console.WriteLine(students[i].Telefon);
+}
+
+// Console.WriteLine(students.Count);
+
+
+
 #endregion
 
 
